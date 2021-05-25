dc48864ceff1d5c110cdb79818beb25d634937af
yashgoenka01 <goenka.yash0@gmail.com>
2021-05-25 21:08:45 +0545

    nothing

---
+++ src/com/flipkart/service/ProfessorOperation.java
@@ -0,0 +1,38 @@
+  
+/**
+ * 
+ */
+package com.flipkart.service;
+
+import java.util.List;
+
+import com.flipkart.bean.Course;
+
+/**
+ * @author Goenka
+ *
+ */
+public class ProfessorOperation implements ProfessorInterface {
+
+	@Override
+	public boolean addGrade(String studentID, String courseID, String grade) 
+	{
+		// 
+		return false;
+	}
+
+	@Override
+	//the type should be RegisteredCourse
+	public List<String> viewEnrolledStudent(String courseID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	//the type should be Course
+	public List<String> viewCourses(String profID) 
+	{
+		return null;
+	}
+
+}
