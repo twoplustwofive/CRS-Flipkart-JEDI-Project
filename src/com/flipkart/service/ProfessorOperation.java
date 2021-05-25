598adae9703011da09c1a6d3f4e65785a7e32c3b
yashgoenka01 <goenka.yash0@gmail.com>
2021-05-25 21:00:28 +0545

    Added UserDao

--- src/com/flipkart/service/ProfessorOperation.java
+++ src/com/flipkart/service/ProfessorOperation.java
@@ -5,11 +5,111 @@ package com.flipkart.service;
 
 import java.util.List;
 
+import com.flipkart.bean.RegisteredCourse;
+
 /**
  * @author Goenka
  *
  */
 public class ProfessorOperation implements ProfessorInterface {
+	private static volatile ProfessorOperation instance=null;
+	
+	ProfessorDaoInterface professorDAOInterface=ProfessorDaoOperation.getInstance();
+	private ProfessorOperation()
+	{
+		
+	}
+	
+	/**
+	 * Method to make ProfessorOperation Singleton
+	 * @return
+	 */
+	public static ProfessorOperation getInstance()
+	{
+		if(instance==null)
+		{
+			// This is a synchronized block, when multiple threads will access this instance
+			synchronized(ProfessorOperation.class){
+				instance=new ProfessorOperation();
+			}
+		}
+		return instance;
+	}
+	
+	
+	/**
+	 * Method to grade a Student
+	 * @param studentId
+	 * @param courseCode
+	 * @param grade
+	 * @return boolean indicating if grade is added or not
+	 * @throws GradeNotAddedException
+	 */
+	@Override
+	public boolean addGrade(int studentId,String courseCode,String grade) throws GradeNotAddedException {
+		try
+		{
+			professorDAOInterface.addGrade(studentId, courseCode, grade);
+		}
+		catch(Exception ex)
+		{
+			throw new GradeNotAddedException(studentId);
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * Method to view all the enrolled students
+	 * @param profId: professor id 
+	 * @return List of enrolled students
+	 */
+	@Override
+	public List<EnrolledStudent> viewEnrolledStudents(String profId) throws SQLException{
+		List<EnrolledStudent> enrolledStudents=new ArrayList<EnrolledStudent>();
+		try
+		{
+			enrolledStudents=professorDAOInterface.getEnrolledStudents(profId);
+		}
+		catch(Exception ex)
+		{
+			throw ex;
+		}
+		return enrolledStudents;
+	}
+
+	
+	/**
+	 * Method to get list of all course a professor is teaching
+	 * @param profId: professor id 
+	 * @return List of courses the professor is teaching
+	 */
+	@Override
+	public List<Course> getCourses(String profId) {
+		//call the DAO class
+		//get the courses for the professor
+		List<Course> coursesOffered=new ArrayList<Course>();
+		try
+		{
+			coursesOffered=professorDAOInterface.getCoursesByProfessor(profId);
+		}
+		catch(Exception ex)
+		{
+			throw ex;
+		}
+		return coursesOffered;
+	}
+	
+	/**
+	 * Method to get the professor name with ID
+	 * @param profId
+	 * @return Professor name 
+	 */
+	@Override
+	public String getProfessorById(String profId)
+	{
+		return professorDAOInterface.getProfessorById(profId);
+	}
 
 	@Override
 	public boolean addGrade(String studentID, String courseID, String grade) {
@@ -18,15 +118,7 @@ public class ProfessorOperation implements ProfessorInterface {
 	}
 
 	@Override
-	//the type should be RegisteredCourse
-	public List<String> viewEnrolledStudent(String courseID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	//the type should be Course
-	public List<String> getCourses(String profID) {
+	public List<RegisteredCourse> viewEnrolledStudent(String courseID) {
 		// TODO Auto-generated method stub
 		return null;
 	}
