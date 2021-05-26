ec426d17067dc3d5227760b400dfe62f222f3d46
TN2000 <tanishq17nandan@gmail.com>
2021-05-26 13:20:41 +0530

    admindao fixed

--- src/com/flipkart/client/AdminCRSMenu.java
+++ src/com/flipkart/client/AdminCRSMenu.java
@@ -193,7 +193,7 @@ public class AdminCRSMenu {
 		}
 		System.out.println(String.format("%20s | %20s | %20s | %20s", "UserId", "StudentId", "Name", "Gender"));
 		for(Student student : pendingStudentsList) {
-			System.out.println(String.format("%20s | %20d | %20s | %20s", student.getUserID(), student.getStudentID(), student.getName(), student.getGender().toString()));
+			System.out.println(String.format("%20s | %20d | %20s | %20s", student.getUserID(), student.getStudentId(), student.getName(), student.getGender().toString()));
 		}
 		return pendingStudentsList;
 	}
